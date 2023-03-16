87634d6202b096d11cef700d854ce90764c251a1
TomixUG <tomas.sousek1@gmail.com>
2023-03-16 01:24:34 +0100

    blink the led when no device is connected

--- src/main.cpp
+++ src/main.cpp
@@ -14,14 +14,23 @@ Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 #define SWITCH 2
 #define LED 33
 
+unsigned long ledPreviousMillis = 0; // will store last time LED was updated
+const long ledInterval = 500;        // interval at which to blink (milliseconds)
+int ledState = LOW;                  // stores wheter the led is on or off
+
 bool mode = false;
 // A (false) - normal
 // B (true) - gaming
 
-bool keyStates[4] = {false, false, false, false};            // temporary storage for the key states
-int keyPins[4] = {RIGHT_UP, RIGHT_DOWN, LEFT_UP, LEFT_DOWN}; // the key pins on the microcontroller
-uint8_t keyCodesA[4] = {32, 33, 27, 14};                     // keyCodes for mode A
-uint8_t keyCodesB[4] = {18, 5, 17, 16};                      // keyCodes for mode B
+bool keyStates[4] = {false, false, false, false};                // temporary storage for the key states
+int keyPins[4] = {RIGHT_UP, RIGHT_DOWN, LEFT_UP, LEFT_DOWN};     // the key pins on the microcontroller
+uint8_t keyCodesA[4] = {BUTTON_2, BUTTON_1, BUTTON_4, BUTTON_5}; // keyCodes for mode A
+// 5 - Y
+// 2 - B
+// 1 - A
+// 4 - X
+
+uint8_t keyCodesB[4] = {18, 5, 17, 16}; // keyCodes for mode B
 
 float mapf(float x, float in_min, float in_max, float out_min, float out_max)
 {
@@ -67,7 +76,6 @@ void setup()
     pinMode(SWITCH, INPUT_PULLUP);
 
     pinMode(LED, OUTPUT);
-    digitalWrite(LED, HIGH);
 
     Serial.println("Starting BLE work!");
 
@@ -98,17 +106,22 @@ void loop()
 
     if (bleGamepad.isConnected())
     {
-        // switching the modes
-        if (digitalRead(SWITCH) == HIGH)
-            mode = false;
-        else
-            mode = true;
+        // if device is connected turn the led on
+        digitalWrite(LED, HIGH);
 
         // the joystick
-        if (mode == false)
-            bleGamepad.setRightThumb(mapf(event.acceleration.x, -10.3, 10.8, 32767, 0), mapf(event.acceleration.y, -11, 9.6, 0, 32767));
-        else
+        if (digitalRead(SWITCH) == LOW)
+        {
+            mode = false;
             bleGamepad.setLeftThumb(mapf(event.acceleration.x, -10.3, 10.8, 32767, 0), mapf(event.acceleration.y, -11, 9.6, 0, 32767));
+            bleGamepad.setRightThumb(16383, 16383); // reset the other joystick to be in  the midle
+        }
+        else
+        {
+            mode = true;
+            bleGamepad.setRightThumb(mapf(event.acceleration.x, -10.3, 10.8, 32767, 0), mapf(event.acceleration.y, -11, 9.6, 0, 32767));
+            bleGamepad.setLeftThumb(16383, 16383); // reset the other joystick to be in the midle
+        }
 
         bleGamepad.sendReport();
 
@@ -118,16 +131,29 @@ void loop()
             handleButton(counter);
         }
     }
+    else
+    {
+        // if device is not connected, blink the LED
+        unsigned long currentMillis = millis();
+        if (currentMillis - ledPreviousMillis >= ledInterval)
+        {
+            ledPreviousMillis = currentMillis; // save the last time the led blinked
 
-    /* Display the results (acceleration is measured in m/s^2) */
-    Serial.print("X: ");
-    Serial.print(event.acceleration.x);
-    Serial.print(" ");
-    Serial.print("Y: ");
-    Serial.print(event.acceleration.y);
-    Serial.print(" ");
-    Serial.print("Z: ");
-    Serial.print(event.acceleration.z);
-    Serial.print(" ");
-    Serial.println("m/s^2 ");
+            // if the LED is off turn it on and vice-versa:
+            if (ledState == LOW)
+            {
+                ledState = HIGH;
+            }
+            else
+            {
+                ledState = LOW;
+            }
+
+            // set the LED with the ledState of the variable:
+            digitalWrite(LED, ledState);
+        }
+    }
+
+    // show accelerometer data
+    Serial.println("X: " + String(event.acceleration.x) + "   Y: " + String(event.acceleration.y) + "   Z: " + String(event.acceleration.z));
 }
